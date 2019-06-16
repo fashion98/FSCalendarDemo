d6660f72e4217fd8c580aedc32d4f6423cd6e3b1
樊盛 <870587568@qq.com>
2019-06-16 20:28:42 +0800

    Update ViewController.m
    加上回今天的调用

--- FSCalendarDemo/FSCalendarDemo/ViewController.m
+++ FSCalendarDemo/FSCalendarDemo/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "FSCalendarView.h"
+#import "NSDate+FSCalendar.h"
 
 @interface ViewController ()<FSCalendarDelegate>
 
@@ -19,12 +20,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // 回今天
+    [self initRightBarbutton];
+    
+    // 初始化日历
+    [self initCalendarView];
+}
 
+- (void)initRightBarbutton {
+    
+    UIBarButtonItem *rightButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"今天" style:(UIBarButtonItemStylePlain) target:self action:@selector(refreshToCurrentDate)];
+    self.navigationItem.rightBarButtonItem = rightButtonItem;
+}
+
+- (void)initCalendarView {
+    
     // demo 的 frame 我就简单随便一写，项目中根据实际给值即可
     self.calendarView = [[FSCalendarView alloc] initWithFrame:CGRectMake(0, 88, kScreenWidth, kScreenHeight-88) withCalendarHeight:kScreenWidth*6/7+kCalendarHeaderViewHeight withShowSingle:YES];
     self.calendarView.fsDelegate = self;
     [self.view addSubview:self.calendarView];
+    
+}
 
+// 回今天
+- (void)refreshToCurrentDate {
+    
+    [self.calendarView refreshToCurrentDate];
 }
 
 - (void)calendarDidSelectedWithDate:(NSDate *)date {
@@ -37,7 +59,7 @@
     NSMutableArray *arr = [NSMutableArray array];
     for (int i = 0; i < 5; i++) {
         NSInteger random = arc4random()%31+1;
-        [arr addObject:[NSString stringWithFormat:@"2019-05-%02d", random]];
+        [arr addObject:[NSString stringWithFormat:@"%zd-%02zd-%02zd", [NSDate date].dateYear, [NSDate date].dateMonth, random]];
     }
     
     // 赋值pointsArray
@@ -54,3 +76,4 @@
 
 
 @end
+
